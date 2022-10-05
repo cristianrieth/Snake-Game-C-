9ab8f93d0472ff79fc03e30582ce230b50d3c24a
Cristian Rieth <113387764+cristianrieth@users.noreply.github.com>
2022-10-05 00:29:13 -0300

    Add files via upload

---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PnTela = new System.Windows.Forms.Panel();
+            this.LbPontos = new System.Windows.Forms.Label();
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarJogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Frame = new System.Windows.Forms.Timer(this.components);
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnTela
+            // 
+            this.PnTela.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.PnTela.Location = new System.Drawing.Point(12, 67);
+            this.PnTela.Name = "PnTela";
+            this.PnTela.Size = new System.Drawing.Size(428, 428);
+            this.PnTela.TabIndex = 0;
+            // 
+            // LbPontos
+            // 
+            this.LbPontos.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.LbPontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbPontos.Location = new System.Drawing.Point(12, 31);
+            this.LbPontos.Name = "LbPontos";
+            this.LbPontos.Size = new System.Drawing.Size(428, 33);
+            this.LbPontos.TabIndex = 1;
+            this.LbPontos.Text = "Pontos: 0";
+            this.LbPontos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Menu
+            // 
+            this.Menu.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(448, 24);
+            this.Menu.TabIndex = 2;
+            this.Menu.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarJogoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // iniciarJogoToolStripMenuItem
+            // 
+            this.iniciarJogoToolStripMenuItem.Name = "iniciarJogoToolStripMenuItem";
+            this.iniciarJogoToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.iniciarJogoToolStripMenuItem.Text = "Iniciar jogo";
+            this.iniciarJogoToolStripMenuItem.Click += new System.EventHandler(this.iniciarJogoToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Frame
+            // 
+            this.Frame.Tick += new System.EventHandler(this.Frame_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 498);
+            this.Controls.Add(this.LbPontos);
+            this.Controls.Add(this.PnTela);
+            this.Controls.Add(this.Menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.Menu;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake II";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Clicado);
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnTela;
+        private System.Windows.Forms.Label LbPontos;
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarJogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Timer Frame;
+    }
+}
+
